53fecd1b326ce1577c715c72157ece5ce0d8851f
Swpee <s.suwapee@gmail.com>
2024-05-23 20:05:00 +0700

    add: JavaScript Values Variable Assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,17 @@
 //Exercise #1: Variable Creation
 //Start coding here
 
+let personName = "Karter";
+let personSurname = "Anderson";
+let personAddress = "54/24 Gp5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const billTotal = 15000;
+const restaurantAmount = 1500;
+
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+personAddress = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+billTotal = 5000;
+restaurantAmount = 2500;
